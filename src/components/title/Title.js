9151b63f1f09e486160458ab1ca0d4fc9dcf1026
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-04-20 14:34:46 +0200

    First Commit

---
+++ src/components/title/Title.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Title = () => {
+    return <h1>Mini Reddit App</h1>;
+};
---
+++ src/containers/headerComponent/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Title } from "../../components/title/Title";
+
+export const Header = () => {
+    return (
+        <div>
+            <section>
+                <Title />
+            </section>
+        </div>
+    );
+};
