367a0f57c2a69dcc02a414141b77e972ba2ebd4e
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-04-20 22:04:32 +0200

    Add reddit container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import { Header } from "./containers/headerComponent/Header";
+import { Reddits } from "./containers/redditsComponent/Reddits";
 
 function App() {
     const [subreddit, setSubreddit] = useState("");
@@ -10,6 +11,7 @@ function App() {
             <header className="App-header">
                 <Header />
             </header>
+            <Reddits />
         </div>
     );
 }
