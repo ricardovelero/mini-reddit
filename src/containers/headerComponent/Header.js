6aebc0208fabc0b845403455fd33a02a53793ca7
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-04-24 22:33:46 +0200

    Add select and layout

--- src/containers/headerComponent/Header.js
+++ src/containers/headerComponent/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Title } from "../../components/title/Title";
+import { Select } from "../../components/select/Select";
 
 export const Header = () => {
     return (
-        <div>
-            <section>
-                <Title />
-            </section>
+        <div className="flex items-center justify-betweens">
+            <Title />
+            <Select />
         </div>
     );
 };
