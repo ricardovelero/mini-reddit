c13a7ae52bb21fc5f265681ab009e9adb65bb2bc
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-04-26 22:33:09 +0200

    Remove div and add classes to combobox input

--- src/components/select/Select.js
+++ src/components/select/Select.js
@@ -39,19 +39,18 @@ export const Select = () => {
                     Select Subreddit:
                 </Combobox.Label>
                 <div className="relative mt-1">
-                    <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-                        <Combobox.Input
-                            className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-                            displayValue={(subreddit) => subreddit.title}
-                            onChange={(event) => setQuery(event.target.value)}
+                    <Combobox.Input
+                        className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-12 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        displayValue={(subreddit) => subreddit?.title}
+                        onChange={(event) => setQuery(event.target.value)}
+                    />
+                    <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                        <ChevronUpDownIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
                         />
-                        <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-                            <ChevronUpDownIcon
-                                className="h-5 w-5 text-gray-400"
-                                aria-hidden="true"
-                            />
-                        </Combobox.Button>
-                    </div>
+                    </Combobox.Button>
+
                     <Transition
                         as={Fragment}
                         leave="transition ease-in duration-100"
